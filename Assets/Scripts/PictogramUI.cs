017ef8a60c603219b6bd09defb9d7eded3d3d783
Roxane Braconnier <101722065+R0xane@users.noreply.github.com>
2026-01-27 00:19:45 +0100

    pictogrammes + canva err/corr

--- Assets/Scripts/PictogramUI.cs
+++ Assets/Scripts/PictogramUI.cs
@@ -6,6 +6,12 @@ public class PictogramUI : MonoBehaviour
     public GameObject pictogramCanvas;
     private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grab;
 
+    void Start()
+    {
+        pictogramCanvas.SetActive(false);
+    }
+
+
     void Awake()
     {
         grab = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
