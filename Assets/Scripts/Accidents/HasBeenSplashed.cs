92f59a1d0780aba6dd7389e44f9dc4dbe86e82a0
Roxane Braconnier <101722065+R0xane@users.noreply.github.com>
2026-01-28 00:57:41 +0100

    removed the sliders to be easier

--- Assets/Scripts/Accidents/HasBeenSplashed.cs
+++ Assets/Scripts/Accidents/HasBeenSplashed.cs
@@ -4,27 +4,9 @@ using UnityEngine.UI;
 public class HasBeenSplashed : MonoBehaviour
 {
     private SplashOnGrab splashOnGrab;
-    public Slider splashSlider;
-
     [Header("Exposure")]
-    public float exposureLevel = 0f;
-    public float exposureIncreasePerSecond = 5f;
-    public float maxExposure = 100f;
-    public bool continueExposure = false;
     private SplashOnGrab[] splashScripts;
     public AudioSource audioSource;
-
-    void Start()
-    {
-        splashSlider.gameObject.SetActive(false);
-
-        if (splashSlider != null)
-        {
-            splashSlider.minValue = 0f;
-            splashSlider.maxValue = maxExposure;   
-            splashSlider.value = exposureLevel;
-        }
-    }
 
 
     void Update()
@@ -32,50 +14,12 @@ public class HasBeenSplashed : MonoBehaviour
         splashScripts = FindObjectsOfType<SplashOnGrab>();
 
         if (splashScripts != null && splashScripts.Length > 0 && splashScripts[0] != null){
-            Debug.Log("Checking splash status...");
-
-            if (splashScripts[0].splashed)
-            {
-                continueExposure = true;
-            }
+            return;
         }
         else {
             audioSource.Stop();
         }
-        if (continueExposure)
-        {
-            StartingSplashExposure();
-            splashSlider.gameObject.SetActive(true);
-            
-        }
-    }
-        
 
-    private void StartingSplashExposure()
-    {
-        exposureLevel += exposureIncreasePerSecond * Time.deltaTime;
-        exposureLevel = Mathf.Clamp(exposureLevel, 0f, maxExposure);
-
-        if (splashSlider != null)
-            splashSlider.value = exposureLevel;
-
-        // Optionnel : quand c'est max
-        if (Mathf.Approximately(exposureLevel, maxExposure))
-        {
-            Debug.Log("Exposure max !");
-
-        }
-    }
-
-    // Optionnel : reset si douche / nettoyage
-    public void ResetExposure()
-    {
-        exposureLevel = 0f;
-        if (splashSlider != null)
-            splashSlider.value = exposureLevel;
-
-        if (splashScripts != null && splashScripts.Length > 0 && splashScripts[0] != null)
-            splashScripts[0].splashed = false; // si tu veux reset le status
     }
 
 }
