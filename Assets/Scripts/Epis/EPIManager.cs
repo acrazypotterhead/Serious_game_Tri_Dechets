2bf0af4bf22bb4059ab2037b3ec9c0519baca554
Rolwen <n.rnolwen@gmail.com>
2026-01-27 13:42:07 +0100

    Added sound when all EPI selected

--- Assets/Scripts/Epis/EPIManager.cs
+++ Assets/Scripts/Epis/EPIManager.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class EPIManager : MonoBehaviour
 {
     public static EPIManager Instance { get; private set; }
 
     public int selectedEPIIndex = 0;
+    public int totalEPI = 3;
+    public TMP_Text epiCounterText;
     public TMP_Text epiText;
+
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip allEpiSelectedClip;
+
+    private bool allEpiDone = false;
 
     private void Awake()
     {
@@ -16,6 +25,11 @@ public class EPIManager : MonoBehaviour
     private void Start()
     {
         selectedEPIIndex = 0;
+        if (epiCounterText != null)
+        {
+            epiCounterText.gameObject.SetActive(true);
+            UpdateEpiText();
+        }
     }
 
 
@@ -24,9 +38,33 @@ public class EPIManager : MonoBehaviour
         selectedEPIIndex++;
         Debug.Log($"EPI selected: {selectedEPIIndex}/3");
 
-        if (selectedEPIIndex >= 3){
+        UpdateEpiText();
+
+        if (selectedEPIIndex >= totalEPI && !allEpiDone)
+        {
+            allEpiDone = true;
+
             Debug.Log("All EPIs have been selected!");
-            epiText.text = "ALL EPIs have been selected!";
+
+            if (epiCounterText != null)
+                epiText.text = "ALL EPIs have been selected!";
+                StartCoroutine(HideMessageAfterDelay());
+
+            if (audioSource != null && allEpiSelectedClip != null)
+                audioSource.PlayOneShot(allEpiSelectedClip);
         }
+    }
+
+    IEnumerator HideMessageAfterDelay() 
+    { 
+        yield return new WaitForSeconds(4f); 
+        epiCounterText.gameObject.SetActive(false);
+        epiText.gameObject.SetActive(false);
+    }
+
+    private void UpdateEpiText()
+    {
+        if (epiCounterText != null)
+            epiCounterText.text = "EPI worn: "+ selectedEPIIndex+"/"+totalEPI;
     }
 }
